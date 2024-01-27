89b89e0b37084e4dae38abdc12626bcf385360a8
sbx_user1051 <sbx_user1051@169.254.0.141>
2024-01-27 12:38:53 +0000

    Sat Jan 27 12:38:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,16 @@
+from table import Table
 from item import Item
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # Init price table and offers
+    itemA = Item("A",50,3,130)
+    itemB = Item("B",30,2,45)
+    itemC = Item("C",20,0,0)
+    itemD = Item("D",15,0,0)
+    items = [itemA,itemB,itemC,itemD]
+    table = Table(items)
+    # calculate checkout
+    return skus
+    
+
--- lib/solutions/CHK/item.py
+++ lib/solutions/CHK/item.py
@@ -1,5 +1,9 @@
 class Item:
-    def __init__(self,ID,price,offers):
+    def __init__(self,ID,price,offerAmount,OfferPrice):
         self.ID=ID
         self.price=price
-        self.offers=offers
+        self.offerAmount=offerAmount
+        self.OfferPrice=OfferPrice
+    
+    def calculateDiscount(self,quantity):
+        return 0

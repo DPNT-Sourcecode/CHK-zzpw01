66c2c97ebafb827bcdebb0eb4fa693958db70074
sbx_user1051 <sbx_user1051@169.254.0.141>
2024-01-27 12:47:53 +0000

    Sat Jan 27 12:47:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,13 @@
-from item import Item
+
 # noinspection PyUnusedLocal
 # skus = unicode string
+from lib.solutions.CHK.table import item
+
+
 def checkout(skus):
     # Init price table and offers
     itemA = Item("A",50,3,130)
-    itemB = Item("B",30,2,45)
+    itemB = item("B",30,2,45)
     itemC = Item("C",20,0,0)
     itemD = Item("D",15,0,0)
     items = [itemA,itemB,itemC,itemD]
